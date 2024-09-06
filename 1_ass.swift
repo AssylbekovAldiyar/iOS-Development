49d52f41a924a72ecd5379ee0a7134f715dac4db
AssylbekovAldiyar <123085249+AssylbekovAldiyar@users.noreply.github.com>
2024-09-06 10:43:01 +0500

    Add files via upload

---
+++ 1_ass.swift
@@ -0,0 +1,17 @@
+
+var firstName = "Assylbekov ", lastname = "Aldiyar"
+var age = 20, born = 2003
+var isstud = true
+var height = 177.55
+let curyear = 2023
+var bla: Bool? = nil
+if age == curyear - born{
+    bla = true
+}
+var hobby = "i like to read something."
+var numofhub = 3
+var fovnum = 10
+var ishobcre = true
+var me="my name is \(firstName + lastname), i am \(age) years old, i am current student, \(hobby), which is a creative hobby, i have \(numofhub),and my fov num is \(fovnum)."
+var dop = "In the future, I want to become a professional iOS developer."
+print(me + dop)
