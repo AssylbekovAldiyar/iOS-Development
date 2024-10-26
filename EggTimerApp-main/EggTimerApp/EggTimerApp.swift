89cf3b96bb8ab02688fb3c53878ddcf8c86cddf7
AssylbekovAldiyar <123085249+AssylbekovAldiyar@users.noreply.github.com>
2024-10-26 14:12:46 +0500

    Add files via upload

---
+++ EggTimerApp-main/EggTimerApp/EggTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggTimerApp.swift
+//  EggTimerApp
+//
+//  Created by Zholbarys on 25.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct EggTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EggTimerView()
+        }
+    }
+}
